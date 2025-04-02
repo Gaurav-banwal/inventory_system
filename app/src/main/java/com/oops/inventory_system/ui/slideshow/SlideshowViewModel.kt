23003a31f160d7580100303491ea932d2ee40783
Gaurav <gauravbanwal1234@gmail.com>
2025-04-02 19:16:46 +0530

    Initial commit

---
+++ app/src/main/java/com/oops/inventory_system/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.oops.inventory_system.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
