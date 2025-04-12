4283efd9d3eb30a62a5b99a778ccd81028225cd2
Gaurav <gauravbanwal1234@gmail.com>
2025-04-12 11:51:16 +0530

    more work 4

---
+++ app/src/main/java/com/oops/inventory_system/Item.kt
@@ -0,0 +1,7 @@
+package com.oops.inventory_system
+
+data class Item(
+    val trackId: Int,
+    var quantity: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/oops/inventory_system/ItemAdaptor.kt
@@ -0,0 +1,48 @@
+package com.oops.inventory_system
+
+import android.content.ClipData
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.TextView
+
+
+
+class ItemAdapter(private val items: MutableList<Item>, private val context: Context) : BaseAdapter() {
+
+    override fun getCount(): Int = items.size
+    override fun getItem(position: Int): Any = items[position]
+    override fun getItemId(position: Int): Long = items[position].trackId.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        val item = items[position]
+
+        val nameText = view.findViewById<TextView>(R.id.itemName)
+        val quantityText = view.findViewById<TextView>(R.id.quantity)
+        val addButton = view.findViewById<Button>(R.id.addButton)
+        val subtractButton = view.findViewById<Button>(R.id.subtractButton)
+
+        nameText.text = item.name
+        quantityText.text = item.quantity.toString()
+
+        addButton.setOnClickListener {
+            item.quantity++
+            quantityText.text = item.quantity.toString()
+            notifyDataSetChanged() // Refresh ListView
+        }
+
+        subtractButton.setOnClickListener {
+            if (item.quantity > 0) {
+                item.quantity--
+                quantityText.text = item.quantity.toString()
+                notifyDataSetChanged()
+            }
+        }
+
+        return view
+    }
+}
